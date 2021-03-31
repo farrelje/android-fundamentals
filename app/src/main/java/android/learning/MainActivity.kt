34c3f25209e6e90706a6a01ae2552b2a329f9a9a
farrelje <farrel.burns@gmail.com>
2021-04-01 09:10:19 +1300

    ImageView

--- app/src/main/java/android/learning/MainActivity.kt
+++ app/src/main/java/android/learning/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.edittext.*
+import kotlinx.android.synthetic.main.imageview.*
 import kotlinx.android.synthetic.main.textview.*
 import java.util.*
 
@@ -13,23 +14,11 @@ class MainActivity : AppCompatActivity() {
     // CTRL + ALT + L to auto-format code
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.edittext)
-        // Old Java
-        //val btnApply = findViewById<Button>(R.id.btnSubmit)
-        btnAdd.setOnClickListener {
-            var firstNum: Int?
-            var secondNumber: Int?
-            try {
-                firstNum = etNum1.text.toString().toInt()
-                secondNumber =  etNum2.text.toString().toInt()
-                val result = firstNum + secondNumber
-                tvResult.text = result.toString()
-            } catch (e: NumberFormatException) {
-                Log.d("MainActivity","$e - Please use valid numbers only")
-            }
-            // string can go to editable, but not other way around
-        }
+        setContentView(R.layout.imageview)
 
+        btnAddImage.setOnClickListener {
+            ivImage.setImageResource(R.drawable.front)
+        }
 
     }
 
